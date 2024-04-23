fda0e648996d9dc7d51d4ccab4402e9fcafd8f42
hi021 <haxordp@gmail.com>
2024-04-23 22:58:18 +0200

    tiny cleanup

--- src/routes/(navgroup)/about/+page.svelte
+++ src/routes/(navgroup)/about/+page.svelte
@@ -1,132 +1,143 @@
-<script lang="ts">
-	export const csr = false;
-</script>
-
 <svelte:head>
-	<title>about - poggers</title>
+  <title>about - poggers</title>
 </svelte:head>
 
 <main>
-	<h1>hii</h1>
-	<p style="margin-bottom: 44px;">
-		This is an ongoing rewrite of a rewrite of pogowanie - a site that tracks osu! score
-		leaderboards.
-		<br />
-		Most of the functionality is already implemented, but some of it lacks polish. I'll be working on
-		that!
-	</p>
+  <h1>hii</h1>
+  <p style="margin-bottom: 44px;">
+    This is an ongoing rewrite of a rewrite of pogowanie - a site that tracks
+    osu! score leaderboards.
+    <br />
+    Most of the functionality is already implemented, but some of it lacks polish.
+    I'll be working on that!
+  </p>
 
-	<h2>What's different?</h2>
-	<p class="para">
-		The site and API were remade from scratch to hopefully be much more performant and nicer to use.
-		<br />
-		I've also moved away from osu!stats to respektive's API.
-		<br />
-		<br />
-		The biggest change is the inclusion of tracking for top 25, top 8, and top 1 scores. There are also
-		user profiles and the site is in dark mode!
-	</p>
+  <h2>What's different?</h2>
+  <p class="para">
+    The site and API were remade from scratch to hopefully be much more
+    performant and nicer to use.
+    <br />
+    I've also moved away from osu!stats to respektive's API.
+    <br />
+    <br />
+    The biggest change is the inclusion of tracking for top 25, top 8, and top 1
+    scores. There are also user profiles and the site is in dark mode!
+  </p>
 
-	<h2>Credits</h2>
-	<p class="para">
-		90% of credit goes to respektive and <a
-			class="a"
-			href="https://osustats.respektive.pw"
-			target="_blank"
-			rel="noreferrer">osustats.respektive.pw</a
-		>, the other 10% goes to stack overflow.
-		<br />
-		The site also uses images from
-		<a class="a" href="https://osu.ppy.sh/" target="_blank" rel="noreferrer">osu.ppy.sh</a>
-		and flag icons from
-		<a class="a" href="https://github.com/lipis/flag-icons" target="_blank" rel="noreferrer"
-			>this repository</a
-		>.
-	</p>
+  <h2>Credits</h2>
+  <p class="para">
+    90% of credit goes to respektive and <a
+      class="a"
+      href="https://osustats.respektive.pw"
+      target="_blank"
+      rel="noreferrer">osustats.respektive.pw</a
+    >, the other 10% goes to stack overflow.
+    <br />
+    The site also uses images from
+    <a class="a" href="https://osu.ppy.sh/" target="_blank" rel="noreferrer"
+      >osu.ppy.sh</a
+    >
+    and flag icons from
+    <a
+      class="a"
+      href="https://github.com/lipis/flag-icons"
+      target="_blank"
+      rel="noreferrer">this repository</a
+    >.
+  </p>
 
-	<h2>Contact</h2>
-	<p class="para">
-		Scream at <code>hi#8112</code> in case something breaks;;<br />
-		Ask me or <code>respektive#1895</code> if in need of more detailed data that the site doesn't show
-	</p>
+  <h2>Contact</h2>
+  <p class="para">
+    Scream at <code>hi#8112</code> in case something breaks;;<br />
+    Ask me or <code>respektive#1895</code> if in need of more detailed data that
+    the site doesn't show
+  </p>
 
-	<h2>osu! score ranking archive</h2>
-	<h4>Why are there no scores at a given date?</h4>
-	<p class="para">
-		Probably because the API was down for the entire day and it wasn't possible to save the ranking.
-		<br />
-		Top 25s, 8s, and 1s have been tracked since October 9th 2022 and there is no data beyond that date.
-		<br />
-		<br />
-		If you're looking at today's ranking, that becomes available around 10 a.m. UTC.
-		<br /><small
-			>respektive's API is still quite unstable, so the time may drastically vary from day to day.</small
-		>
-	</p>
-	<h4>How does it work?</h4>
-	<p class="para">barely\</p>
+  <h2>osu! score ranking archive</h2>
+  <h4>Why are there no scores at a given date?</h4>
+  <p class="para">
+    Probably because the API was down for the entire day and it wasn't possible
+    to save the ranking.
+    <br />
+    Top 25s, 8s, and 1s have been tracked since October 9th 2022 and there is no
+    data beyond that date.
+    <br />
+    <br />
+    If you're looking at today's ranking, that becomes available around 10 a.m. UTC.
+    <br /><small
+      >respektive's API is still quite unstable, so the time may drastically
+      vary from day to day.</small
+    >
+  </p>
+  <h4>How does it work?</h4>
+  <p class="para">barely\</p>
 
-	<h2>poggers API</h2>
-	<p class="para">
-		The site's API is not private so you can probably use it. Maybe.<br />
-		Basic documentation can be found <a href="/api">here</a>.
-	</p>
+  <h2>poggers API</h2>
+  <p class="para">
+    The site's API is not private so you can probably use it. Maybe.<br />
+    Basic documentation can be found <a href="/api">here</a>.
+  </p>
 
-	<h2>Other osu! stat-tracking sites</h2>
-	<ul>
-		<li>
-			<strong><a href="https://osustats.respektive.pw/">respektive's osu!stats</a></strong> - the site
-			where all the data comes from with the full ranking including people below 1000 top 50s
-		</li>
-		<li>
-			<strong><a href="https://osustats.ppy.sh/">osu!Stats</a></strong> - the original site for score
-			rankings
-		</li>
-		<li>
-			<strong><a href="https://snipe.huismetbenen.nl/">osu!snipe</a></strong> - for country #1s
-		</li>
-		<li>
-			<strong><a href="https://ameobea.me/osutrack/">osu!track</a></strong> - profile stats tracking
-			with satisfying charts
-		</li>
-		<li>
-			<strong><a href="https://osudaily.net">osu!daily</a></strong> - lots of rankings like playcount,
-			SS count, etc. + daily profile stats tracking similar to osu!track
-		</li>
-		<li>
-			<strong><a href="https://osuskills.com">osu!Skills</a></strong> - pp ranking but even more broken
-		</li>
-	</ul>
+  <h2>Other osu! stat-tracking sites</h2>
+  <ul>
+    <li>
+      <strong
+        ><a href="https://osustats.respektive.pw/">respektive's osu!stats</a
+        ></strong
+      > - the site where all the data comes from with the full ranking including
+      people below 1000 top 50s
+    </li>
+    <li>
+      <strong><a href="https://osustats.ppy.sh/">osu!Stats</a></strong> - the original
+      site for score rankings
+    </li>
+    <li>
+      <strong><a href="https://snipe.huismetbenen.nl/">osu!snipe</a></strong> - for
+      country #1s
+    </li>
+    <li>
+      <strong><a href="https://ameobea.me/osutrack/">osu!track</a></strong> - profile
+      stats tracking with satisfying charts
+    </li>
+    <li>
+      <strong><a href="https://osudaily.net">osu!daily</a></strong> - lots of rankings
+      like playcount, SS count, etc. + daily profile stats tracking similar to osu!track
+    </li>
+    <li>
+      <strong><a href="https://osuskills.com">osu!Skills</a></strong> - pp ranking
+      but even more broken
+    </li>
+  </ul>
 </main>
 
 <style>
-	main {
-		padding: 0 3.5%;
-	}
+  main {
+    padding: 0 3.5%;
+  }
 
-	h1 {
-		font-weight: 700;
-	}
-	h2 {
-		margin-bottom: -2px;
-		font-weight: 400;
-		background-color: rgba(0, 0, 0, 0.3);
-		padding: 8px;
-	}
+  h1 {
+    font-weight: 700;
+  }
+  h2 {
+    margin-bottom: -2px;
+    font-weight: 400;
+    background-color: rgba(0, 0, 0, 0.3);
+    padding: 8px;
+  }
 
-	.para {
-		padding-inline-start: 1rem;
-	}
+  .para {
+    padding-inline-start: 1rem;
+  }
 
-	a {
-		color: var(--color-active);
-	}
-	.a {
-		color: #999;
-	}
-	code {
-		color: var(--color-active);
-		background-color: rgba(0, 0, 0, 0.15);
-		padding: 4px;
-	}
+  a {
+    color: var(--color-active);
+  }
+  .a {
+    color: #999;
+  }
+  code {
+    color: var(--color-active);
+    background-color: rgba(0, 0, 0, 0.15);
+    padding: 4px;
+  }
 </style>
--- src/routes/api/+page.svelte
+++ src/routes/api/+page.svelte
@@ -1,347 +1,375 @@
-<script lang="ts">
-	export const csr = false;
-</script>
-
 <svelte:head>
-	<title>api - poggers</title>
+  <title>api - poggers</title>
 </svelte:head>
 
 <main>
-	<h1>poggers api</h1>
-	<p>
-		This API is not private but it sure is a mess
-		<br />
-		Yell at <code>hi#8112</code> in case it explodes
-		<br />
-		<small>server smol please be gentle;;</small>
-	</p>
-	<small>v3.0 (2022-11-06)</small>
+  <h1>poggers api</h1>
+  <p>
+    This API is not private but it sure is a mess
+    <br />
+    Yell at <code>hi#8112</code> in case it explodes
+    <br />
+    <small>server smol please be gentle;;</small>
+  </p>
+  <small>v3.0 (2022-11-06)</small>
 
-	<h2>Types</h2>
-	<span class="type-name">PlayerRanking</span>
-	<ul>
-		<li>
-			<span>date</span>
-			<code class="type-type">string</code>
-			<span class="description">YYYY-MM-DD day from which the data was taken</span>
-		</li>
-		<li>
-			<span>value</span>
-			<code class="type-type">number</code>
-			<span class="description">amount of scores</span>
-		</li>
-		<li>
-			<span>rank</span>
-			<code class="type-type">number</code>
-			<span class="description">global rank in given scores ranking</span>
-		</li>
-		<li>
-			<span>countryRank</span>
-			<code class="type-type">number</code>
-		</li>
-		<li>
-			<span>gained</span>
-			<code class="type-type">number | undefined</code>
-			<span class="description"
-				>amount of scores gained in the last `gainedDays` days, undefined if not in previous ranking
-				archive</span
-			>
-		</li>
-		<li>
-			<span>gainedRank</span>
-			<code class="type-type">number | undefined</code>
-			<span class="description"
-				>amount of ranks gained in the last `gainedDays` days, undefined if not in previous ranking
-				archive</span
-			>
-		</li>
-		<li>
-			<span>gainedDays</span>
-			<code class="type-type">number | undefined</code>
-			<span class="description">defaults to 1, only set in case of a gap in archive entries</span>
-		</li>
-		<li>
-			<span>mostGained</span>
-			<code
-				>{'{date: '}<code class="type-type">string</code>, value:
-				<code class="type-type">number</code>{'}'}</code
-			>
-			<span class="description">value is most gained scores in one day, date is YYYY-MM-DD</span>
-		</li>
-		<li>
-			<span>peak</span>
-			<code
-				>{'{date: '}<code class="type-type">string</code>, value:
-				<code class="type-type">number</code>{'}'}</code
-			>
-			<span class="description">highest score count</span>
-		</li>
-		<li>
-			<span>lowest</span>
-			<code
-				>{'{date: '}<code class="type-type">string</code>, value:
-				<code class="type-type">number</code>{'}'}</code
-			>
-			<span class="description">lowest score count</span>
-		</li>
-	</ul>
+  <h2>Types</h2>
+  <span class="type-name">PlayerRanking</span>
+  <ul>
+    <li>
+      <span>date</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >YYYY-MM-DD day from which the data was taken</span
+      >
+    </li>
+    <li>
+      <span>value</span>
+      <code class="type-type">number</code>
+      <span class="description">amount of scores</span>
+    </li>
+    <li>
+      <span>rank</span>
+      <code class="type-type">number</code>
+      <span class="description">global rank in given scores ranking</span>
+    </li>
+    <li>
+      <span>countryRank</span>
+      <code class="type-type">number</code>
+    </li>
+    <li>
+      <span>gained</span>
+      <code class="type-type">number | undefined</code>
+      <span class="description"
+        >amount of scores gained in the last `gainedDays` days, undefined if not
+        in previous ranking archive</span
+      >
+    </li>
+    <li>
+      <span>gainedRank</span>
+      <code class="type-type">number | undefined</code>
+      <span class="description"
+        >amount of ranks gained in the last `gainedDays` days, undefined if not
+        in previous ranking archive</span
+      >
+    </li>
+    <li>
+      <span>gainedDays</span>
+      <code class="type-type">number | undefined</code>
+      <span class="description"
+        >defaults to 1, only set in case of a gap in archive entries</span
+      >
+    </li>
+    <li>
+      <span>mostGained</span>
+      <code
+        >{"{date: "}<code class="type-type">string</code>, value:
+        <code class="type-type">number</code>{"}"}</code
+      >
+      <span class="description"
+        >value is most gained scores in one day, date is YYYY-MM-DD</span
+      >
+    </li>
+    <li>
+      <span>peak</span>
+      <code
+        >{"{date: "}<code class="type-type">string</code>, value:
+        <code class="type-type">number</code>{"}"}</code
+      >
+      <span class="description">highest score count</span>
+    </li>
+    <li>
+      <span>lowest</span>
+      <code
+        >{"{date: "}<code class="type-type">string</code>, value:
+        <code class="type-type">number</code>{"}"}</code
+      >
+      <span class="description">lowest score count</span>
+    </li>
+  </ul>
 
-	<span class="type-name">PlayerInfo</span>
-	<ul>
-		<li>
-			<span>_id</span>
-			<code class="type-type">number</code>
-			<span class="description">osu! id</span>
-		</li>
-		<li>
-			<span>name</span>
-			<code class="type-type">string</code>
-			<span class="description">current osu! username</span>
-		</li>
-		<li>
-			<span>country</span>
-			<code class="type-type">string</code>
-			<span class="description">uppercase 2-letter country code</span>
-		</li>
-		<li>
-			<span>oldName</span>
-			<code class="type-type">string[]</code>
-			<span class="description">previous osu! usernames (from when the player was tracked)</span>
-		</li>
-	</ul>
+  <span class="type-name">PlayerInfo</span>
+  <ul>
+    <li>
+      <span>_id</span>
+      <code class="type-type">number</code>
+      <span class="description">osu! id</span>
+    </li>
+    <li>
+      <span>name</span>
+      <code class="type-type">string</code>
+      <span class="description">current osu! username</span>
+    </li>
+    <li>
+      <span>country</span>
+      <code class="type-type">string</code>
+      <span class="description">uppercase 2-letter country code</span>
+    </li>
+    <li>
+      <span>oldName</span>
+      <code class="type-type">string[]</code>
+      <span class="description"
+        >previous osu! usernames (from when the player was tracked)</span
+      >
+    </li>
+  </ul>
 
-	<span class="type-name">RankingCategory</span>
-	<ul>
-		<li>
-			<code class="type-type">'top50' | 'top25' | 'top8' | 'top1'</code>
-		</li>
-	</ul>
+  <span class="type-name">RankingCategory</span>
+  <ul>
+    <li>
+      <code class="type-type">'top50' | 'top25' | 'top8' | 'top1'</code>
+    </li>
+  </ul>
 
-	<span class="type-name">Player</span>
-	<ul>
-		<li>
-			<code class="type-type"
-				>{'PlayerInfo & { [ranking in RankingCategory]?: PlayerRanking }'}</code
-			>
-		</li>
-	</ul>
+  <span class="type-name">Player</span>
+  <ul>
+    <li>
+      <code class="type-type"
+        >{"PlayerInfo & { [ranking in RankingCategory]?: PlayerRanking }"}</code
+      >
+    </li>
+  </ul>
 
-	<span class="type-name">CountryRanking</span>
-	<p class="description-main">Used for country rankings at /api/ranking/countries</p>
-	<ul>
-		<li>
-			<span>country</span>
-			<code class="type-type">string</code>
-			<span class="description">uppercase 2-letter country code</span>
-		</li>
-		<li>
-			<span>total</span>
-			<code class="type-type">number</code>
-			<span class="description">total amount of given scores for the country</span>
-		</li>
-		<li>
-			<span>players</span>
-			<code class="type-type">number</code>
-			<span class="description">total amount of players tracked in the country</span>
-		</li>
-		<li>
-			<span>average</span>
-			<code class="type-type">number</code>
-			<span class="description">average amount of given scores for the country</span>
-		</li>
-		<li>
-			<span>weighted</span>
-			<code class="type-type">number</code>
-			<span class="description">weighted amount of given scores for the country</span>
-		</li>
-	</ul>
+  <span class="type-name">CountryRanking</span>
+  <p class="description-main">
+    Used for country rankings at /api/ranking/countries
+  </p>
+  <ul>
+    <li>
+      <span>country</span>
+      <code class="type-type">string</code>
+      <span class="description">uppercase 2-letter country code</span>
+    </li>
+    <li>
+      <span>total</span>
+      <code class="type-type">number</code>
+      <span class="description"
+        >total amount of given scores for the country</span
+      >
+    </li>
+    <li>
+      <span>players</span>
+      <code class="type-type">number</code>
+      <span class="description"
+        >total amount of players tracked in the country</span
+      >
+    </li>
+    <li>
+      <span>average</span>
+      <code class="type-type">number</code>
+      <span class="description"
+        >average amount of given scores for the country</span
+      >
+    </li>
+    <li>
+      <span>weighted</span>
+      <code class="type-type">number</code>
+      <span class="description"
+        >weighted amount of given scores for the country</span
+      >
+    </li>
+  </ul>
 
-	<span class="type-name">MostGainedRanking</span>
-	<p class="description-main">Used for most gained rankings at /api/gains</p>
-	<ul>
-		<li>
-			<span>_id</span>
-			<code class="type-type">number</code>
-			<span class="description">ranking position (1-99)</span>
-		</li>
-		<li>
-			<span>id</span>
-			<code class="type-type">number</code>
-			<span class="description">player osu! id</span>
-		</li>
-		<li>
-			<span>name</span>
-			<code class="type-type">string</code>
-			<span class="description">osu! username</span>
-		</li>
-		<li>
-			<span>country</span>
-			<code class="type-type">string</code>
-			<span class="description">uppercase 2-letter country code</span>
-		</li>
-		<li>
-			<span>value</span>
-			<code class="type-type">number</code>
-			<span class="description">number of scores total from the day after the gains</span>
-		</li>
-		<li>
-			<span>gained</span>
-			<code class="type-type">number</code>
-			<span class="description">number of scores gained in 1 day</span>
-		</li>
-		<li>
-			<span>date</span>
-			<code class="type-type">string</code>
-			<span class="description">YYYY-MM-DD</span>
-		</li>
-	</ul>
+  <span class="type-name">MostGainedRanking</span>
+  <p class="description-main">Used for most gained rankings at /api/gains</p>
+  <ul>
+    <li>
+      <span>_id</span>
+      <code class="type-type">number</code>
+      <span class="description">ranking position (1-99)</span>
+    </li>
+    <li>
+      <span>id</span>
+      <code class="type-type">number</code>
+      <span class="description">player osu! id</span>
+    </li>
+    <li>
+      <span>name</span>
+      <code class="type-type">string</code>
+      <span class="description">osu! username</span>
+    </li>
+    <li>
+      <span>country</span>
+      <code class="type-type">string</code>
+      <span class="description">uppercase 2-letter country code</span>
+    </li>
+    <li>
+      <span>value</span>
+      <code class="type-type">number</code>
+      <span class="description"
+        >number of scores total from the day after the gains</span
+      >
+    </li>
+    <li>
+      <span>gained</span>
+      <code class="type-type">number</code>
+      <span class="description">number of scores gained in 1 day</span>
+    </li>
+    <li>
+      <span>date</span>
+      <code class="type-type">string</code>
+      <span class="description">YYYY-MM-DD</span>
+    </li>
+  </ul>
 
-	<hr />
+  <hr />
 
-	<h2>Endpoints</h2>
-	<h4 class="endpoint-url">
-		/api/ranking/<span class="emphasis">players</span>/[date]/[category]/[country]/[ranks]
-		<span class="method get" />
-	</h4>
-	<h5>Route parameters (positional)</h5>
-	<ul>
-		<li>
-			<span>date</span>
-			<code class="type-type">string</code>
-			<span class="description">YYYY-MM-DD date or "latest"; minimum date is 2020-05-10</span>
-		</li>
-		<li>
-			<span>category</span>
-			<code class="type-type">RankingCategory</code>
-			<span class="description">score category</span>
-		</li>
-		<li>
-			<span>country</span>
-			<code class="type-type">string</code>
-			<span class="description"
-				>2-letter country code to show players only from given country or "all"</span
-			>
-		</li>
-		<li>
-			<span>ranks</span>
-			<code class="type-type">string</code>
-			<span class="description"
-				>min-max rank/score ranking position (e.g. 1-100) or 0-0 to show all players</span
-			>
-		</li>
-	</ul>
-	<h5>Returns</h5>
-	<code>Array&lt;<code class="type-type">Player</code>&gt;</code>
+  <h2>Endpoints</h2>
+  <h4 class="endpoint-url">
+    /api/ranking/<span class="emphasis">players</span
+    >/[date]/[category]/[country]/[ranks]
+    <span class="method get" />
+  </h4>
+  <h5>Route parameters (positional)</h5>
+  <ul>
+    <li>
+      <span>date</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >YYYY-MM-DD date or "latest"; minimum date is 2020-05-10</span
+      >
+    </li>
+    <li>
+      <span>category</span>
+      <code class="type-type">RankingCategory</code>
+      <span class="description">score category</span>
+    </li>
+    <li>
+      <span>country</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >2-letter country code to show players only from given country or "all"</span
+      >
+    </li>
+    <li>
+      <span>ranks</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >min-max rank/score ranking position (e.g. 1-100) or 0-0 to show all
+        players</span
+      >
+    </li>
+  </ul>
+  <h5>Returns</h5>
+  <code>Array&lt;<code class="type-type">Player</code>&gt;</code>
 
-	<h4 class="endpoint-url">
-		/api/ranking/<span class="emphasis">gains</span>/[date]/[category]/[country]/[ranks]/[...days]
-		<span class="method get" />
-	</h4>
-	<h5>Route parameters (positional)</h5>
-	<ul>
-		<li>
-			<span>date</span>
-			<code class="type-type">string</code>
-			<span class="description">YYYY-MM-DD date or "latest"; minimum date is 2020-05-10</span>
-		</li>
-		<li>
-			<span>category</span>
-			<code class="type-type">RankingCategory</code>
-			<span class="description">score category</span>
-		</li>
-		<li>
-			<span>country</span>
-			<code class="type-type">string</code>
-			<span class="description"
-				>2-letter country code to show players only from given country or "all"</span
-			>
-		</li>
-		<li>
-			<span>ranks</span>
-			<code class="type-type">string</code>
-			<span class="description"
-				>min-max rank/score ranking position (e.g. 1-100) or 0-0 to show all players</span
-			>
-		</li>
-		<li>
-			<span>days</span>
-			<code class="type-type">number</code>
-			<span class="description"
-				>number of days to go back (defaults to 1), will return an empty array if there is no
-				archive entry given days ago</span
-			>
-		</li>
-	</ul>
-	<h5>Returns</h5>
-	<code>Array&lt;<code class="type-type">Player</code>&gt;</code>
+  <h4 class="endpoint-url">
+    /api/ranking/<span class="emphasis">gains</span
+    >/[date]/[category]/[country]/[ranks]/[...days]
+    <span class="method get" />
+  </h4>
+  <h5>Route parameters (positional)</h5>
+  <ul>
+    <li>
+      <span>date</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >YYYY-MM-DD date or "latest"; minimum date is 2020-05-10</span
+      >
+    </li>
+    <li>
+      <span>category</span>
+      <code class="type-type">RankingCategory</code>
+      <span class="description">score category</span>
+    </li>
+    <li>
+      <span>country</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >2-letter country code to show players only from given country or "all"</span
+      >
+    </li>
+    <li>
+      <span>ranks</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >min-max rank/score ranking position (e.g. 1-100) or 0-0 to show all
+        players</span
+      >
+    </li>
+    <li>
+      <span>days</span>
+      <code class="type-type">number</code>
+      <span class="description"
+        >number of days to go back (defaults to 1), will return an empty array
+        if there is no archive entry given days ago</span
+      >
+    </li>
+  </ul>
+  <h5>Returns</h5>
+  <code>Array&lt;<code class="type-type">Player</code>&gt;</code>
 
-	<h4 class="endpoint-url">
-		/api/ranking/<span class="emphasis">countries</span>/[date]/[category]/[country]/[ranks]
-		<span class="method get" />
-	</h4>
-	<h5>Route parameters (positional)</h5>
-	<ul>
-		<li>
-			<span>date</span>
-			<code class="type-type">string</code>
-			<span class="description">YYYY-MM-DD date or "latest"; minimum date is 2020-05-10</span>
-		</li>
-		<li>
-			<span>category</span>
-			<code class="type-type">RankingCategory</code>
-			<span class="description">score category</span>
-		</li>
-		<li>
-			<span>country</span>
-			<code class="type-type">string</code>
-			<span class="description"
-				>2-letter country code to show players only from given country or "all"</span
-			>
-		</li>
-		<li>
-			<span>ranks</span>
-			<code class="type-type">string</code>
-			<span class="description"
-				>min-max rank/score ranking position (e.g. 1-100) or 0-0 to show all players</span
-			>
-		</li>
-	</ul>
-	<h5>Returns</h5>
-	<code>Array&lt;<code class="type-type">CountryRanking</code>&gt;</code>
+  <h4 class="endpoint-url">
+    /api/ranking/<span class="emphasis">countries</span
+    >/[date]/[category]/[country]/[ranks]
+    <span class="method get" />
+  </h4>
+  <h5>Route parameters (positional)</h5>
+  <ul>
+    <li>
+      <span>date</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >YYYY-MM-DD date or "latest"; minimum date is 2020-05-10</span
+      >
+    </li>
+    <li>
+      <span>category</span>
+      <code class="type-type">RankingCategory</code>
+      <span class="description">score category</span>
+    </li>
+    <li>
+      <span>country</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >2-letter country code to show players only from given country or "all"</span
+      >
+    </li>
+    <li>
+      <span>ranks</span>
+      <code class="type-type">string</code>
+      <span class="description"
+        >min-max rank/score ranking position (e.g. 1-100) or 0-0 to show all
+        players</span
+      >
+    </li>
+  </ul>
+  <h5>Returns</h5>
+  <code>Array&lt;<code class="type-type">CountryRanking</code>&gt;</code>
 </main>
 
 <style>
-	main {
-		padding: 0.5rem 2.5%;
-	}
+  main {
+    padding: 0.5rem 2.5%;
+  }
 
-	.description-main {
-		font-size: 0.9375rem;
-		opacity: 0.9;
-	}
-	.description {
-		font-size: 0.875rem;
-	}
-	.type-name {
-		font-weight: 500;
-		color: #80cbc4;
-	}
-	.type-type {
-		padding: 3px;
-		background-color: rgba(0, 0, 0, 0.3);
-	}
-	.method {
-		padding: 5px;
-		border-radius: 999px;
-	}
-	.method.get {
-		background-color: #558b2f;
-	}
-	.method.get::after {
-		content: 'GET';
-	}
+  .description-main {
+    font-size: 0.9375rem;
+    opacity: 0.9;
+  }
+  .description {
+    font-size: 0.875rem;
+  }
+  .type-name {
+    font-weight: 500;
+    color: #80cbc4;
+  }
+  .type-type {
+    padding: 3px;
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+  .method {
+    padding: 5px;
+    border-radius: 999px;
+  }
+  .method.get {
+    background-color: #558b2f;
+  }
+  .method.get::after {
+    content: "GET";
+  }
 
-	.emphasis {
-		color: var(--color-active);
-	}
+  .emphasis {
+    color: var(--color-active);
+  }
 </style>
